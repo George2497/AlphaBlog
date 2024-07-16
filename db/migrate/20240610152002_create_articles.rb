d8421c891692e011eb79598f492fe713d5c17153
George Koundouri <georgekoundouri@msn.com>
2024-07-16 18:48:48 +0100

    Adding Articles Show Functionality

--- db/migrate/20240610152002_create_articles.rb
+++ db/migrate/20240610152002_create_articles.rb
@@ -2,9 +2,7 @@ class CreateArticles < ActiveRecord::Migration[7.1]
   def change
     create_table :articles do |t|
       t.string :title
-      t.string :description
-
-      t.timestamps
+      t.text :description
     end
   end
 end
