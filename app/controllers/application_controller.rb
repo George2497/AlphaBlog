5cc6c2ada199d3a7e8e689c405bcdb0552d4a2db
George Koundouri <georgekoundouri@msn.com>
2024-08-21 18:41:19 +0100

    Logged in users can now make their own articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    # If we have current_user then return the @current_user instance variable
+    # if we do not have the current_user then do the query to find the user
+    # and to then return them
+    # This is done by using ||= If true, use the value on the left, else use the value on the right
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user # Makes current_user a boolean to see if they're logged in or not by using !! (Double bang)
+  end
+
 end
