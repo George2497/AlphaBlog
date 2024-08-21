4bbd5a682535b38c20e65267f415f54dda4c417c
George Koundouri <georgekoundouri@msn.com>
2024-08-21 18:19:34 +0100

    Authentication helper methods made for the Application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   def gravatar_for(user,options = {size: 80})
     email_address = user.email.downcase
     hash = Digest::MD5.hexdigest(email_address)
@@ -6,4 +7,17 @@ module ApplicationHelper
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
+
+  def current_user
+    # If we have current_user then return the @current_user instance variable
+    # if we do not have the current_user then do the query to find the user
+    # and to then return them
+    # This is done by using ||= If true, use the value on the left, else use the value on the right
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user # Makes current_user a boolean to see if they're logged in or not by using !! (Double bang)
+  end
+
 end
